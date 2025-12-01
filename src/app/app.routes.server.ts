64a95351a267eba24e8bc87982691966b4ae22bc
bprcode <bprcode@gmail.com>
2025-11-30 17:50:52 -0800

    Add product prerender list

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,21 @@
-import { RenderMode, ServerRoute } from '@angular/ssr';
+import { inject } from '@angular/core'
+import { RenderMode, ServerRoute } from '@angular/ssr'
+import { ProductService } from './product-service'
+import { firstValueFrom } from 'rxjs'
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'products/:id',
+    renderMode: RenderMode.Prerender,
+    async getPrerenderParams() {
+      const service = inject(ProductService)
+      const list = await firstValueFrom(service.list)
+      return list.map(p => ({id: p.id}))
+    },
+
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
-];
+    renderMode: RenderMode.Prerender,
+  },
+]
