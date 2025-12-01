30151718f4bf46e7d6dde1d3529f4d9946f9dbf6
bprcode <bprcode@gmail.com>
2025-11-30 21:31:36 -0800

    Add ping status check

--- .github/triggers/ping-trigger.sh
+++ .github/triggers/ping-trigger.sh
@@ -1,10 +1,16 @@
 echo "Attempting POST to ping endpoint"
 source ../.secret/ping-tester-token.env
-curl -L \
-  -v \
+status=$(curl -s -o /dev/null -w "%{http_code}" -L \
   -X POST \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer $GITHUB_TOKEN" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   https://api.github.com/repos/ScintillaTesting/showcase-builder/actions/workflows/web-ping.yml/dispatches \
-  -d '{"ref":"actions-experiment","inputs":{}}'
+  -d '{"ref":"actions-experiment","inputs":{}}')
+echo "got status: $status"
+if [[ "$status" -ge 200 && "$status" -lt 300 ]]; then
+    echo "✔️📬 POST request to action succeeded with status $status"
+else
+    echo "❌📭 POST failed with status $status"
+    exit 1
+fi
