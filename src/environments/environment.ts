17c20843df11f038aa034527a2ae3490f0bc72ef
bprcode <bprcode@gmail.com>
2025-12-01 14:02:09 -0800

    Try moving concat

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   mode: '📽️ Production Environment',
   based(url: string) {
+    console.log('debug: returning:',`/showcase-builder${url}`)
     return `/showcase-builder${url}`
   },
 }
